49dac32d6361bc2c58bb831daab2a0bc357231c7
alonhrri <alonhrri@gmail.com>
2023-11-14 12:51:33 +0200

    Add Logs files

---
+++ TestRating/Logs/LogMessages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRating
+{
+    public static class LogMessages
+    {
+        public const string SystemStart = "Insurance Rating System Starting...";
+        public const string RateStart = "Starting rate.";
+        public const string LoadingPolicy = "Loading policy.";
+        public const string ValidateStart = "Validating policy.";
+        public const string UnknownRate = "Unknown policy type";
+        public const string RateComplete = "Rating completed.";
+        public const string RateTypeFormat = "Rating {0} policy...";
+        public const string ErrorFormat = "{0}";
+        public const string RateProduced = "Rating: {0}"; 
+        public const string NoRateProduced = "No rating produced.";
+
+    }
+}
---
+++ TestRating/Logs/Logger.cs
@@ -0,0 +1,30 @@
+﻿
+namespace TestRating
+{
+    public class Logger : ILogger
+    {       
+        public void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+        }
+
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+        }
+
+        public void LogSuccess(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+        }
+    }
+}
